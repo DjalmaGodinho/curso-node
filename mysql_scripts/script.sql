eebd52c34bbeb7f955267b6aea63c1c71274a8b0
djalmalg <djalma56godinho@gmail.com>
2023-10-31 09:25:15 -0300

    criação de scripts para o banco de dados

---
+++ mysql_scripts/script.sql
@@ -0,0 +1,10 @@
+CREATE TABLE usuarios(
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(100),
+    idade INT
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES("Carlos Bueno", "carlos@teste.com.br", 10);
+INSERT INTO usuarios(nome, email, idade) VALUES("Maria Bueno", "Maria@teste.com.br", 18);
+INSERT INTO usuarios(nome, email, idade) VALUES("Joana Bueno", "carlos@teste.com.br", 20);
+INSERT INTO usuarios(nome, email, idade) VALUES("Joao Bueno", "carlos@teste.com.br", 30);
